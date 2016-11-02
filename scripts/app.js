54ee1f25ffe210c40563cfe6368009a7fde1d577
virginiachae <chae.virginia@gmail.com>
2016-11-01 20:15:15 -0700

    finished giffaw exercise

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,43 @@
 $(document).on("ready", function(){
+  $.ajax({
+    method: 'GET',
+    url: 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC',
+    dataType: 'json',
+    success: onSuccess
+  });
+
+  function onSuccess(json) {
+      for (var i = 0; i < json.data.length; i++) {
+        var imgs = json.data[i].images.fixed_height.url
+        var $newDiv = $("<div class='gifs'><img src='" + imgs + "' /></div>");
+        $('div.gif-gallery').append($newDiv);
+        // console.log(imgs)
+      }
+  };
+  var $data = $("form").serialize();
+
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      method: 'GET',
+      url: 'http://api.giphy.com/v1/gifs/search',
+      dataType: 'json',
+      data: $("form").serialize(),
+      success: onSuccessSearch
+
+    });
+
+  });
+
+function onSuccessSearch(json) {
+  $('div.gifs').remove();
+  for (var i = 0; i < json.data.length; i++) {
+    var searchGifs = json.data[i].images.fixed_height.url
+    var $newDiv = $("<div class='gifs'><img src='" + searchGifs + "' /></div>");
+    $('div.gif-gallery').append($newDiv);
+  }
+
+};
+
 
 });
